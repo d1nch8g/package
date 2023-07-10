d797c35babb729622323a89a6c3e2433e3637dd0
dancheg97 <dancheg97@fmnx.su>
2023-07-10 15:52:57 +0300

    updated gitignore and installation preparation script

--- prepare.sh
+++ prepare.sh
@@ -8,7 +8,6 @@ pack -d . -Pw localhost:3000/dancheg97/testpkg
 wget http://localhost:3000/api/packages/dancheg97/arch/archlinux/x86_64/dancheg97.localhost.db
 cd ..
 
-
 cd testpkg2
 makepkg -sfri --sign --noconfirm
 echo -n dancheg97package$(date --rfc-3339=seconds | tr " " T) >> md
@@ -16,3 +15,8 @@ gpg --detach-sign md
 pack -d . -Pw localhost:3000/dancheg97/testpkg
 wget http://localhost:3000/api/packages/dancheg97/arch/archlinux/any/dancheg97.localhost.db.tar.gz
 cd ..
+
+sudo rm -rf testpkg/pkg
+sudo rm -rf testpkg/src
+sudo rm -rf testpkg2/pkg
+sudo rm -rf testpkg2/src
