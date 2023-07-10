870da9a948cd52d1d1eb0b2ce698f1e89ad41b73
dancheg97 <dancheg97@fmnx.su>
2023-07-10 14:49:29 +0300

    added script for full to fully recreate package and database locally

---
+++ prepare.sh
@@ -0,0 +1,18 @@
+# Local gitea instance should be running and have dancheg97 user for gitea tests.
+
+cd testpkg
+makepkg -sfri --sign --noconfirm
+echo -n dancheg97package$(date --rfc-3339=seconds | tr " " T) >> md
+gpg --detach-sign md
+pack -d . -Pw localhost:3000/dancheg97/testpkg
+wget http://localhost:3000/api/packages/dancheg97/arch/archlinux/x86_64/dancheg97.localhost.db
+cd ..
+
+
+cd testpkg2
+makepkg -sfri --sign --noconfirm
+echo -n dancheg97package$(date --rfc-3339=seconds | tr " " T) >> md
+gpg --detach-sign md
+pack -d . -Pw localhost:3000/dancheg97/testpkg
+wget http://localhost:3000/api/packages/dancheg97/arch/archlinux/any/dancheg97.localhost.db.tar.gz
+cd ..
